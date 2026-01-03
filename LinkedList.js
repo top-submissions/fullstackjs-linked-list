5d339b1f08afc95e908c20fe08585f4a6c0ef6ff
Chonky_Seal <MatimotTheTimoters@gmail.com>
2026-01-03 15:19:31 +0800

    feat: add LinkedList class
    what:
    - uses Node class
    - no functions yet

---
+++ LinkedList.js
@@ -0,0 +1,14 @@
+import { Node } from './Node.js';
+
+/**
+ * Represents a singly linked list data structure
+ * @class LinkedList
+ */
+export class LinkedList {
+  /**
+   * Creates a new empty LinkedList
+   */
+  constructor() {
+    this.headNode = null;
+  }
+}
